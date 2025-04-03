f8c43257d0b35a200735cbde7a8fd810c2e30f82
andersonmerten <anderson.merten@gmail.com>
2025-04-03 11:21:11 -0300

    handle likes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,20 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import{Avatar} from './Avatar';
+import { useState } from 'react';
 
 export function Comment(props){
 
+    const [likeCount, setLikeCount] = useState(0);
+
     function handleDeleteComment(){
         props.deleteComment(props.content);
+    }
+
+    const handleLikeComment=()=>{
+        setLikeCount((state) =>{
+            return state+1;
+        });
     }
 
 
@@ -28,8 +37,8 @@ export function Comment(props){
                     <p>{props.content}</p>
                 </div>
                 <footer>
-                    <button type="button">
-                        <ThumbsUp size={24} /> aplaudir <span/> 20
+                    <button onClick={handleLikeComment} type="button">
+                        <ThumbsUp  size={24} /> aplaudir <span/> {likeCount}
                     </button>
                 </footer>
             </div>
